549957df46eed8263160a5250dc4dc27e0f8faca
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 06:59:08 +0700

    Add project files.

---
+++ KodeProduk.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300158
+{
+    class KodeProduk
+    {
+        private static Dictionary<String, String> produkList = new Dictionary<string, string>()
+        {
+            {"Laptop", "E100"},
+            {"Smartphone", "E101"},
+            {"Tablet", "E102"},
+            {"Headset", "E103"},
+            {"Keyboard", "E104"},
+            {"Mouse", "E105"},
+            {"Printer", "E106"},
+            {"Monitor", "E107"},
+            {"Smartwatch", "E108"},
+            {"Kamera", "E109"},
+        };
+
+        public static string GetKodeProduk(String produk)
+        {
+            return produkList.TryGetValue(produk, out string kodeProduk) ? kodeProduk : "Kode Produk tidak ditemukan";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using modul4_103022300158;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Masukkan nama produk: ");
+        string produk = Console.ReadLine();
+
+        string kp = KodeProduk.GetKodeProduk(produk);
+        Console.WriteLine($"Kode Produk {produk}: {kp}");
+    }
+}
